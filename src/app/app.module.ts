11082925aad5e292bfea58868f06945373efdac1
urapun <gb.liquidarrow@gmail.com>
2017-12-26 14:29:36 +0100

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { CalendarPage } from '..pages/calendar/calendar';
+import { CalendarPage } from '../pages/calendar/calendar';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
