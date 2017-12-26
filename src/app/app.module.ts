e3714b953387d8e12a9f3cf991cdd9e2ad964fa6
urapun <gb.liquidarrow@gmail.com>
2017-12-26 14:28:11 +0100

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { CalendarPage } from '..pages/calendar/calendar';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -27,6 +28,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
+    CalendarPage,
     ContactPage,
     HomePage,
     TabsPage
